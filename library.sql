c7742747e8cbf3909cc4ff68f4cd91d7ed1da2a6
Asingura Philip <asinguraphilip@gmail.com>
2025-03-18 22:05:09 +0300

    tables

--- library.sql
+++ library.sql
@@ -1,1 +1,19 @@
+create database library;
 
+use library;
+
+create table books(
+	book_id int primary key,
+    title varchar(100),
+    author varchar(50),
+    year_published int,
+    genre varchar(50)
+);
+
+create table members(
+	member_id int primary key,
+    first_name varchar(50),
+    last_name varchar(50),
+    email varchar(30),
+    phone_number varchar(30)
+);
