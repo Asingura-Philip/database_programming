ccdcbb7842bf737b043ba4e4c36c46c4882cbebe
Asingura Philip <asinguraphilip@gmail.com>
2025-03-25 21:26:50 +0300

    signup

--- test.sql
+++ test.sql
@@ -1,1 +1,13 @@
+create database test;
+use test;
+
+create table signup(
+	id int auto_increment primary key,
+    username varchar(40) not null,
+    email varchar(40) not null,
+    password varchar(30) not null
+);
+
+
+
 
